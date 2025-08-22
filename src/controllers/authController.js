ac3f625e2762a761b8dcd1357db972dd09c86a8a
zadumka <alenahas1987@gmail.com>
2025-08-22 10:08:41 +0300

    add br

---
+++ src/controllers/authController.js
@@ -0,0 +1,97 @@
+import bcrypt from 'bcrypt';
+import createHttpError from 'http-errors';
+import { User } from '../models/user.js';
+import { Session } from '../models/session.js';
+import { createSession, setSessionCookies } from '../services/auth.js';
+
+export const registerUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return next(createHttpError(400, 'Email in use'));
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({
+    email,
+    password: hashedPassword,
+  });
+
+  // Create new session and set cookies
+  const newSession = await createSession(newUser._id);
+  setSessionCookies(res, newSession);
+
+  res.status(201).json(newUser);
+};
+
+export const loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return next(createHttpError(401, 'User not found'));
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    return next(createHttpError(401, 'Invalid credentials'));
+  }
+
+  // Delete old session
+  await Session.deleteOne({ userId: user._id });
+
+  // Create new session and set cookies
+  const newSession = await createSession(user._id);
+  setSessionCookies(res, newSession);
+
+  res.status(200).json(user);
+};
+
+export const logoutUser = async (req, res) => {
+  const { sessionId } = req.cookies;
+
+  if (sessionId) {
+    await Session.deleteOne({ _id: sessionId });
+  }
+
+  res.clearCookie('sessionId');
+  res.clearCookie('accessToken');
+  res.clearCookie('refreshToken');
+
+  res.status(204).send();
+};
+
+export const refreshUserSession = async (req, res, next) => {
+  // Find and check existing session
+  const session = await Session.findOne({
+    _id: req.cookies.sessionId,
+    refreshToken: req.cookies.refreshToken,
+  });
+
+  if (!session) {
+    return next(createHttpError(401, 'Session not found'));
+  }
+
+  const isSessionTokenExpired =
+    new Date() > new Date(session.refreshTokenValidUntil);
+
+  if (isSessionTokenExpired) {
+    return next(createHttpError(401, 'Session token expired'));
+  }
+
+  // Delete old session
+  await Session.deleteOne({
+    _id: req.cookies.sessionId,
+    refreshToken: req.cookies.refreshToken,
+  });
+
+  // Create new session and set cookies
+  const newSession = await createSession(session.userId);
+  setSessionCookies(res, newSession);
+
+  res.status(200).json({
+    message: 'Session refreshed',
+  });
+};
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { celebrate } from 'celebrate';
+import {
+  loginUser,
+  logoutUser,
+  refreshUserSession,
+  registerUser,
+} from '../controllers/authController.js';
+import {
+  loginUserSchema,
+  registerUserSchema,
+} from '../validations/authValidation.js';
+
+const router = Router();
+
+router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+router.post('/auth/logout', logoutUser);
+router.post('/auth/refresh', refreshUserSession);
+
+export default router;
